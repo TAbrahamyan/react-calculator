3a5cdfc4b01a3a0e3e42844a07a68eb5d716a025
Tigran Abrahamyan <tabrahamyan@Tigrans-MacBook-Pro.local>
2020-07-17 17:01:04 +0400

    Complete basic calculation

--- src/components/Calculator.tsx
+++ src/components/Calculator.tsx
@@ -5,50 +5,73 @@ import '../scss/components/_calculator.scss';
 
 export const Calculator:React.FunctionComponent = () => {
   const [ history, setHistory ] = React.useState<string>('');
-  const [ output, setOutput ] = React.useState<string>('0');
+  const [ output, setOutput ] = React.useState<string>('');
 
-  const addNumberHandler = ({ target: { textContent: t } }: any): void => {
-    if (output.startsWith('0')) {
-      setOutput(t);
-      return;
-    }
-
-    setOutput(output + t);
-  }
+  const addNumberHandler = ({ target: { textContent: t } }: any): void => setOutput(output + t);
 
   const operationHandler = ({ target: { textContent: t }}: any): void => {
-    if (t === '=') {
+    if (!history && t === '=') {
+      return;
+    } else if (
+      output
+      && !history
+      && t !== '='
+      && !output.endsWith('.')
+    ) {
+      setHistory(output + t);
+      setOutput('');
+    } else if (
+      history
+      && output
+      && t !== '='
+      && !output.endsWith('.')
+    ) {
+      setHistory((eval(history + output)) + t);
+      setOutput('');
+      return;
+    } else if (
+      history
+      && output
+      && t === '='
+      && !output.endsWith('.')
+    ) {
       setOutput(eval(history + output));
       setHistory('');
       return;
     }
-
-    setHistory(output + t);
-    setOutput('');
   }
 
-  const clearAll = (): void => setOutput('0');
-
-  const removeLast = (): void => {
-    if (output === '0') {
-      return;
+  const outsiderEvents = React.useCallback(({ target: { textContent: t } }: any): void => {
+    if (t === 'C') {
+      setOutput('');
+      setHistory('');
+    } else if (t === 'CE') {
+      setOutput(output.slice(0, -1));
+    } else if (t === '-/+' && !output.endsWith('.')) {
+      setOutput(String(-output));
+    } else if (t === '.' && output && !output.includes('.')) {
+      setOutput(output + '.');
     }
+  }, [ output ]);
 
-    setOutput(output.length === 1 ? output.replace(output.slice(0, 1), '0') : output.slice(0, -1));
-  }
+  React.useEffect(() => {
+    document.addEventListener('click', outsiderEvents);
 
-  const reverse = (): void => setOutput(String(-output));
+    return () => {
+      document.removeEventListener('click', outsiderEvents);
+    }
+  }, [ outsiderEvents ]);
 
   return (
     <div className="calculator">
       <div className="calculator__result">
         <Input value={history} disabled />
-        <Input style={{ color: 'black', fontSize: '1.8em' }} value={output} disabled />
+        <Input value={output} disabled style={{ color: 'black', fontSize: '1.8em' }} />
       </div>
       <div className="calculator__buttons">
-        <Button type="primary" onClick={clearAll}>C</Button>
-        <Button type="primary" onClick={removeLast}>CE</Button>
-        <Button type="primary" onClick={reverse}>-/+</Button>
+        <Button type="primary">C</Button>
+        <Button type="primary">CE</Button>
+        <Button type="primary">-/+</Button>
 
         <Button type="primary" onClick={addNumberHandler}>7</Button>
         <Button type="primary" onClick={addNumberHandler}>8</Button>
